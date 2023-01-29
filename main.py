8b55ddf807a6896161e2064f1e1c3221ceb08b8c
Garius6 <garius642@gmail.com>
2023-01-29 13:09:10 +0300

    Fixed read_task error

--- main.py
+++ main.py
@@ -19,19 +19,19 @@ def get_db():
 
 
 @app.post("/tasks/", response_model=schemas.Task)
-def create_user(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db=db, task=task)
 
 
 @app.get("/tasks/", response_model=list[schemas.Task])
-def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     tasks = crud.get_tasks(db, skip=skip, limit=limit)
     return tasks
 
 
 @app.get("/tasks/{task_id}", response_model=schemas.Task)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_task = crud.get_user(db, task_id=user_id)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = crud.get_task(db, task_id=task_id)
     if db_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return db_task
